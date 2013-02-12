99ff05e69726b57094e8e98691782d81b9951e16
Sheng Wang <newton449@gmail.com>
2013-02-12 12:56:06 -0500

    Init

---
+++ src/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sheng
+ */
+public class NewClass {
+
+    public void method(int i) {
+    }
+}
